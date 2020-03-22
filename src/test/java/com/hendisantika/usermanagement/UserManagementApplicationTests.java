465cfd3041581f1413a4046d59a080ddb2c840e5
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:00:56 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

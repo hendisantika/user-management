845e2c0265a16a873302a4ab7ea09ea50b2a9f4b
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:40:45 +0700

    #40 - Add UserController

---
+++ src/main/java/com/hendisantika/usermanagement/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.hendisantika.usermanagement.controller;
+
+import com.hendisantika.usermanagement.repository.RoleRepository;
+import com.hendisantika.usermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/03/20
+ * Time: 10.39
+ */
+@Controller
+public class UserController {
+
+    private final String TAB_FORM = "formTab";
+    private final String TAB_LIST = "listTab";
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+}

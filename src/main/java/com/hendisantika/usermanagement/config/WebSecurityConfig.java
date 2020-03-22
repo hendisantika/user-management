9100190f27f0e4919d18c88dd52684ae2ba213d2
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:09:28 +0700

    #12 - Add configure in WebSecurityConfig

--- src/main/java/com/hendisantika/usermanagement/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/usermanagement/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hendisantika.usermanagement.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +17,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    String[] resources = new String[]{
+            "/include/**", "/css/**", "/icons/**", "/img/**", "/js/**", "/layer/**"
+    };
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(resources).permitAll()
+                .antMatchers("/", "/index", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/userForm")
+                .failureUrl("/login?error=true")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .csrf().disable()
+                .logout()
+                .permitAll()
+                .logoutSuccessUrl("/login?logout");
+    }
 }

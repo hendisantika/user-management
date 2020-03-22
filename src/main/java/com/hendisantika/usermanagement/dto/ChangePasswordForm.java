61ad49e35573d0e6cec09eddb525d4d0d81aabff
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:08:10 +0700

    #9 - Add ChangePasswordForm DTO

---
+++ src/main/java/com/hendisantika/usermanagement/dto/ChangePasswordForm.java
@@ -0,0 +1,40 @@
+package com.hendisantika.usermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/03/20
+ * Time: 10.07
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ChangePasswordForm {
+    @NotNull
+    private Long id;
+
+    @NotBlank(message = "Current Password must not be blank")
+    private String currentPassword;
+
+    @NotBlank(message = "New Password must not be blank")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm Password must not be blank")
+    private String confirmPassword;
+
+    public ChangePasswordForm(Long id) {
+        this.id = id;
+    }
+}

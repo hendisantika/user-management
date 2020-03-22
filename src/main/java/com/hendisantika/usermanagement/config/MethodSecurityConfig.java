a1438a3eb9ad22f36e1331ca1b5396a885f0e4c8
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:08:48 +0700

    #10 - Add MethodSecurityConfig

---
+++ src/main/java/com/hendisantika/usermanagement/config/MethodSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.hendisantika.usermanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/03/20
+ * Time: 10.08
+ */
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+}

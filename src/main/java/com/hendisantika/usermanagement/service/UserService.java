591af53b949a6e1bc0f267cf7ffa026ecce3c531
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:15:28 +0700

    #15 - Add UserService

--- src/main/java/com/hendisantika/usermanagement/service/UserService.java
+++ src/main/java/com/hendisantika/usermanagement/service/UserService.java
@@ -1,19 +1,9 @@
 package com.hendisantika.usermanagement.service;
 
-import com.hendisantika.usermanagement.entity.Role;
 import com.hendisantika.usermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,28 +12,14 @@ import java.util.Set;
  * Email: hendisantika@gmail.com
  * Telegram : @hendisantika34
  * Date: 22/03/20
- * Time: 10.11
+ * Time: 10.15
  */
 @Service
-@Transactional
-public class UserService implements UserDetailsService {
+public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-        com.hendisantika.usermanagement.entity.User appUser =
-                userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Login " +
-                        "Username Invalido."));
-
-        Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>();
-        for (Role role : appUser.getRoles()) {
-            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getDescription());
-            grantList.add(grantedAuthority);
-        }
-        UserDetails user = new User(username, appUser.getPassword(), grantList);
-
-        return user;
-    }
 }

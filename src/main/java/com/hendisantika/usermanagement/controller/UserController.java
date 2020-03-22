6c5044d96496a40e1b4c17e0e88b0fd1bd3f12a2
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:42:58 +0700

    #43 - Add signupAction in UserController

--- src/main/java/com/hendisantika/usermanagement/controller/UserController.java
+++ src/main/java/com/hendisantika/usermanagement/controller/UserController.java
@@ -2,13 +2,20 @@ package com.hendisantika.usermanagement.controller;
 
 import com.hendisantika.usermanagement.entity.Role;
 import com.hendisantika.usermanagement.entity.User;
+import com.hendisantika.usermanagement.exception.CustomFieldValidationException;
 import com.hendisantika.usermanagement.repository.RoleRepository;
 import com.hendisantika.usermanagement.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +29,7 @@ import java.util.List;
  * Time: 10.39
  */
 @Controller
+@Slf4j
 public class UserController {
 
     private final String TAB_FORM = "formTab";
@@ -43,9 +51,34 @@ public class UserController {
         Role userRole = roleRepository.findByName("USER");
         List<Role> roles = Arrays.asList(userRole);
 
+        log.info("Accesing singup page");
         model.addAttribute("signup", true);
         model.addAttribute("userForm", new User());
         model.addAttribute("roles", roles);
         return "user-form/user-signup";
     }
+
+    @PostMapping("/signup")
+    public String signupAction(@Valid @ModelAttribute("userForm") User user, BindingResult result, ModelMap model) {
+        Role userRole = roleRepository.findByName("USER");
+        List<Role> roles = Arrays.asList(userRole);
+        log.info("Creating user");
+        model.addAttribute("userForm", user);
+        model.addAttribute("roles", roles);
+        model.addAttribute("signup", true);
+
+        if (result.hasErrors()) {
+            return "user-form/user-signup";
+        } else {
+            try {
+                userService.createUser(user);
+            } catch (CustomFieldValidationException cfve) {
+                result.rejectValue(cfve.getFieldName(), null, cfve.getMessage());
+            } catch (Exception e) {
+                model.addAttribute("formErrorMessage", e.getMessage());
+            }
+        }
+        return index();
+    }
+
 }

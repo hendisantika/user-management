70c5f8f0b3efdc34501a43ba60875ea1d32a0e72
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:03:14 +0700

    #3 - Add Role Entity

---
+++ src/main/java/com/hendisantika/usermanagement/entity/Role.java
@@ -0,0 +1,44 @@
+package com.hendisantika.usermanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/03/20
+ * Time: 10.02
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Role implements Serializable {
+    private static final long serialVersionUID = 6353963609310956029L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+}

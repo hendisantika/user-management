426b41763d300f75433d2bdc4e83282102cf861e
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-22 10:16:23 +0700

    #17 - Add checkUsernameAvailable in UserService

--- src/main/java/com/hendisantika/usermanagement/service/UserService.java
+++ src/main/java/com/hendisantika/usermanagement/service/UserService.java
@@ -1,10 +1,13 @@
 package com.hendisantika.usermanagement.service;
 
 import com.hendisantika.usermanagement.entity.User;
+import com.hendisantika.usermanagement.exception.CustomFieldValidationException;
 import com.hendisantika.usermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +28,13 @@ public class UserService {
 
     public Iterable<User> getAllUsers() {
         return repository.findAll();
+    }
+
+    private boolean checkUsernameAvailable(User user) throws Exception {
+        Optional<User> userFound = repository.findByUsername(user.getUsername());
+        if (userFound.isPresent()) {
+            throw new CustomFieldValidationException("Username not available", "username");
+        }
+        return true;
     }
 }
